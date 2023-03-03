9b64ca7a89178d2b65abf2aefde27a006a27fccf
kemoe dokou valy verdy <valyverdykemoe@gmail.com>
2023-03-03 14:39:18 +0100

    Feature: Properties page

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class PropertyController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/properties', name: 'app_property')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'current_menu' => 'home',
+        return $this->render('property/index.html.twig', [
+            'current_menu' => 'properties',
         ]);
     }
 }
